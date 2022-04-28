4d3f67ee309e6b9586d341570c4ed12ae619f209
bodyanlul <55473834+bodyanlul@users.noreply.github.com>
2022-04-28 19:21:29 +0700

    Add files via upload

---
+++ SP3/SP3/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace SP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a11 = new System.Windows.Forms.TextBox();
+            this.a34 = new System.Windows.Forms.TextBox();
+            this.a33 = new System.Windows.Forms.TextBox();
+            this.a32 = new System.Windows.Forms.TextBox();
+            this.a31 = new System.Windows.Forms.TextBox();
+            this.a24 = new System.Windows.Forms.TextBox();
+            this.a23 = new System.Windows.Forms.TextBox();
+            this.a22 = new System.Windows.Forms.TextBox();
+            this.a21 = new System.Windows.Forms.TextBox();
+            this.a14 = new System.Windows.Forms.TextBox();
+            this.a13 = new System.Windows.Forms.TextBox();
+            this.a12 = new System.Windows.Forms.TextBox();
+            this.a44 = new System.Windows.Forms.TextBox();
+            this.a43 = new System.Windows.Forms.TextBox();
+            this.a42 = new System.Windows.Forms.TextBox();
+            this.a41 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.b4 = new System.Windows.Forms.TextBox();
+            this.b3 = new System.Windows.Forms.TextBox();
+            this.b2 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.N = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // a11
+            // 
+            this.a11.Location = new System.Drawing.Point(12, 23);
+            this.a11.Name = "a11";
+            this.a11.Size = new System.Drawing.Size(31, 20);
+            this.a11.TabIndex = 0;
+            this.a11.Text = "0,9";
+            // 
+            // a34
+            // 
+            this.a34.Location = new System.Drawing.Point(123, 75);
+            this.a34.Name = "a34";
+            this.a34.Size = new System.Drawing.Size(31, 20);
+            this.a34.TabIndex = 1;
+            this.a34.Text = "0,02";
+            // 
+            // a33
+            // 
+            this.a33.Location = new System.Drawing.Point(86, 75);
+            this.a33.Name = "a33";
+            this.a33.Size = new System.Drawing.Size(31, 20);
+            this.a33.TabIndex = 2;
+            this.a33.Text = "0,03";
+            // 
+            // a32
+            // 
+            this.a32.Location = new System.Drawing.Point(49, 75);
+            this.a32.Name = "a32";
+            this.a32.Size = new System.Drawing.Size(31, 20);
+            this.a32.TabIndex = 3;
+            this.a32.Text = "0,02";
+            // 
+            // a31
+            // 
+            this.a31.Location = new System.Drawing.Point(12, 75);
+            this.a31.Name = "a31";
+            this.a31.Size = new System.Drawing.Size(31, 20);
+            this.a31.TabIndex = 4;
+            this.a31.Text = "0,93";
+            // 
+            // a24
+            // 
+            this.a24.Location = new System.Drawing.Point(123, 49);
+            this.a24.Name = "a24";
+            this.a24.Size = new System.Drawing.Size(31, 20);
+            this.a24.TabIndex = 5;
+            this.a24.Text = "0,01";
+            // 
+            // a23
+            // 
+            this.a23.Location = new System.Drawing.Point(86, 49);
+            this.a23.Name = "a23";
+            this.a23.Size = new System.Drawing.Size(31, 20);
+            this.a23.TabIndex = 6;
+            this.a23.Text = "0,01";
+            // 
+            // a22
+            // 
+            this.a22.Location = new System.Drawing.Point(49, 49);
+            this.a22.Name = "a22";
+            this.a22.Size = new System.Drawing.Size(31, 20);
+            this.a22.TabIndex = 7;
+            this.a22.Text = "0";
+            // 
+            // a21
+            // 
+            this.a21.Location = new System.Drawing.Point(12, 49);
+            this.a21.Name = "a21";
+            this.a21.Size = new System.Drawing.Size(31, 20);
+            this.a21.TabIndex = 8;
+            this.a21.Text = "0,98";
+            // 
+            // a14
+            // 
+            this.a14.Location = new System.Drawing.Point(123, 23);
+            this.a14.Name = "a14";
+            this.a14.Size = new System.Drawing.Size(31, 20);
+            this.a14.TabIndex = 9;
+            this.a14.Text = "0,02";
+            // 
+            // a13
+            // 
+            this.a13.Location = new System.Drawing.Point(86, 23);
+            this.a13.Name = "a13";
+            this.a13.Size = new System.Drawing.Size(31, 20);
+            this.a13.TabIndex = 10;
+            this.a13.Text = "0,03";
+            // 
+            // a12
+            // 
+            this.a12.Location = new System.Drawing.Point(49, 23);
+            this.a12.Name = "a12";
+            this.a12.Size = new System.Drawing.Size(31, 20);
+            this.a12.TabIndex = 11;
+            this.a12.Text = "0,05";
+            // 
+            // a44
+            // 
+            this.a44.Location = new System.Drawing.Point(123, 101);
+            this.a44.Name = "a44";
+            this.a44.Size = new System.Drawing.Size(31, 20);
+            this.a44.TabIndex = 12;
+            this.a44.Text = "0,03";
+            // 
+            // a43
+            // 
+            this.a43.Location = new System.Drawing.Point(86, 101);
+            this.a43.Name = "a43";
+            this.a43.Size = new System.Drawing.Size(31, 20);
+            this.a43.TabIndex = 13;
+            this.a43.Text = "0";
+            // 
+            // a42
+            // 
+            this.a42.Location = new System.Drawing.Point(49, 101);
+            this.a42.Name = "a42";
+            this.a42.Size = new System.Drawing.Size(31, 20);
+            this.a42.TabIndex = 14;
+            this.a42.Text = "0,03";
+            // 
+            // a41
+            // 
+            this.a41.Location = new System.Drawing.Point(12, 101);
+            this.a41.Name = "a41";
+            this.a41.Size = new System.Drawing.Size(31, 20);
+            this.a41.TabIndex = 15;
+            this.a41.Text = "0,94";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Переходная матрица";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Вектор начального состояния";
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(123, 151);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(31, 20);
+            this.b4.TabIndex = 18;
+            this.b4.Text = "0";
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(86, 151);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(31, 20);
+            this.b3.TabIndex = 19;
+            this.b3.Text = "0";
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(49, 151);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(31, 20);
+            this.b2.TabIndex = 20;
+            this.b2.Text = "0";
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(12, 151);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(31, 20);
+            this.b1.TabIndex = 21;
+            this.b1.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Количество циклов:";
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(122, 178);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(31, 20);
+            this.N.TabIndex = 24;
+            this.N.Text = "10";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 233);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(295, 205);
+            this.richTextBox1.TabIndex = 25;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.a41);
+            this.Controls.Add(this.a42);
+            this.Controls.Add(this.a43);
+            this.Controls.Add(this.a44);
+            this.Controls.Add(this.a12);
+            this.Controls.Add(this.a13);
+            this.Controls.Add(this.a14);
+            this.Controls.Add(this.a21);
+            this.Controls.Add(this.a22);
+            this.Controls.Add(this.a23);
+            this.Controls.Add(this.a24);
+            this.Controls.Add(this.a31);
+            this.Controls.Add(this.a32);
+            this.Controls.Add(this.a33);
+            this.Controls.Add(this.a34);
+            this.Controls.Add(this.a11);
+            this.Name = "Form1";
+            this.Text = "Марковский процесс";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox a11;
+        private System.Windows.Forms.TextBox a34;
+        private System.Windows.Forms.TextBox a33;
+        private System.Windows.Forms.TextBox a32;
+        private System.Windows.Forms.TextBox a31;
+        private System.Windows.Forms.TextBox a24;
+        private System.Windows.Forms.TextBox a23;
+        private System.Windows.Forms.TextBox a22;
+        private System.Windows.Forms.TextBox a21;
+        private System.Windows.Forms.TextBox a14;
+        private System.Windows.Forms.TextBox a13;
+        private System.Windows.Forms.TextBox a12;
+        private System.Windows.Forms.TextBox a44;
+        private System.Windows.Forms.TextBox a43;
+        private System.Windows.Forms.TextBox a42;
+        private System.Windows.Forms.TextBox a41;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox b4;
+        private System.Windows.Forms.TextBox b3;
+        private System.Windows.Forms.TextBox b2;
+        private System.Windows.Forms.TextBox b1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox N;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
